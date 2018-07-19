7e61542d95f7c16b6988aebd60f701e8acf88db7
ali yousefi <aliyr7592@gmail.com>
2018-07-19 17:45:28 +0430

    index.js bug fixed

--- React-com/src/index.js
+++ React-com/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {Button , ButtonPrimary} from './Button';
+import {Button , ButtonPrimary} from './Main';
 
 ReactDom.render(<Button text="ali"/> , document.getElementById('root'));
